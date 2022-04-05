5edf9b2163809efaad5576bb52e85d57aee9eb6e
Max Garat <maxgarat@Maxs-Air.fibertel.com.ar>
2022-04-05 12:54:43 -0300

    correciones puntos 4),5),6)

--- indice/urls.py
+++ indice/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     #path('',otra_vista),
     path('numero-random/',numero_random),#asi la pagina me genera un numero random
     path('dame-numero/<int:numero>',numero_del_usuario),#esto es para que la pagina me pida un numero con el <> le indico el argumento que va a ir en el link, en la direccion, observar que debe como argumento "numero" ya que este es el argumento que tiene la def que esta en este path
-    path('', plantilla)
+    path('plantilla/', plantilla)
+  
     
 
 #el primer argumento del path es para el camino que yo quiero que agarre, ese argumento que yo ponga ahi va a ser como la 'direccion' el codigo clave para que agarre ese camnino. si yo pongo /inicion/ al final del link va a l primer path, si dejo el link original va al ultimo path
